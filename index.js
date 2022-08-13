6ad81b2afe25c87f8f7cab56dec091755a91928a
Arien Woodrow <woodrowar23@gmail.com>
2022-08-13 13:46:41 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var superheroes = require("superheroes");//require the node modules to use them
+var supervillains = require('supervillains');
+
+console.log(superheroes.random());
+console.log(supervillains.random());
